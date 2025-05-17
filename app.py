db1524da841e59ee1d576a96680f6624812dc5c4
Fatima Imran <143261806+fatimaimran22@users.noreply.github.com>
2025-05-17 17:14:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def predict():
     return jsonify({'error': 'Error processing file'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
